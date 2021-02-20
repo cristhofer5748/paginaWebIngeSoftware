3e0505fbfc2a7038e150b90239f43ed66eb5a8fa
cristhofer5748 <cristhofer5047@gmail.com>
2021-02-19 23:26:38 -0600

    ADD: CAMBIO DE RUTAS

--- softwareGt/src/app/app.routing.ts
+++ softwareGt/src/app/app.routing.ts
@@ -4,15 +4,18 @@ import { HomeComponent } from './components/home/home.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import { ContactanosComponent } from './components/contactanos/contactanos.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 const APP_ROUTES: Routes = [
+  {path:'header', component: HeaderComponent },
   {path:'home', component: HomeComponent },
+
   {path:'service', component: ServiceComponent },
   {path:'portafolio', component: PortafolioComponent },
   {path:'nosotros', component: NosotrosComponent },
   {path:'contactanos', component: ContactanosComponent },
-  {path:'**',pathMatch:'full',redirectTo:'home'},
+  {path:'**',pathMatch:'full',redirectTo:'header'},
   {path:'',component:HomeComponent}
 ]
 
