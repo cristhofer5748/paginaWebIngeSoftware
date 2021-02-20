deddceaf7d3a5563eee43095326213fd0dc4c491
Dxander <dsamayoac3@miumg.edu.gt>
2021-02-20 01:27:45 -0600

    ADD: rutas

--- softwareGt/src/app/components/contactanos/contactanos.component.ts
+++ softwareGt/src/app/components/contactanos/contactanos.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contactanos',
   templateUrl: './contactanos.component.html',
-  styleUrls: ['./contactanos.component.css']
+  styleUrls: ['./contactanos.component.css'],
 })
 export class ContactanosComponent implements OnInit {
 
